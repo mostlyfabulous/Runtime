ddbdd915575297783c813dc52c68939e51bc41c5
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-06-04 17:41:34 -0700

    Added Calendar

---
+++ runtime/imports/ui/Calendar.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import interactionPlugin from "@fullcalendar/interaction";
+import googleCalendar from "@fullcalendar/google-calendar";
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+
+// Calendar component -
+export default class Calendar extends Component {
+  render() {
+    return (
+      <div>
+        <FullCalendar defaultView="dayGridMonth"
+        plugins={[ dayGridPlugin ]}
+        events={[
+          { title: 'event 1', date: '2019-04-01' },
+          { title: 'event 2', date: '2019-04-02' }
+          ]}
+        />
+      </div>
+    );
+  }
+}
