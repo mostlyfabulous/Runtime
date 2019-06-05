a1046e8a07bee99c52fff4255a5914a7215086e6
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-06-04 17:41:34 -0700

    Added Calendar

--- runtime/imports/ui/App.jsx
+++ runtime/imports/ui/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
+import Calendar from './Calendar.jsx'
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello />
     <Info />
+    <Calendar />
   </div>
 );
 
