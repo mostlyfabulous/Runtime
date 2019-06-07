623e1026f5d7e0f04e9a1e0b83305d375077c9ca
Napat Karnsakultorn <karns.napat@gmail.com>
2019-06-07 13:35:11 -0700

    API call works now

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import Links from '../api/links';
+const axios = require('axios');
 
 class Info extends Component {
   render() {
-    const links = this.props.links.map(
-      link => this.makeLink(link)
-    );
 
+    this.handleSubmit();
+
+    return (
+      <div>
+        <h2>Learn Meteor!</h2>
+      </div>
+    );
+    /*
     return (
       <div>
         <h2>Learn Meteor!</h2>
         <ul>{ links }</ul>
       </div>
     );
+    */
   }
 
-  makeLink(link) {
-    return (
-      <li key={link._id}>
-        <a href={link.url} target="_blank">{link.title}</a>
-      </li>
-    );
-  }
+
+  handleSubmit() {
+  axios.get('https://api.openweathermap.org/data/2.5/forecast?q=Vancouver,ca&appid=4897c08ee8512a8295bb4bafbd77b966')
+  .then(response => {
+    console.log(response.data.city.name)
+    let localDate = new Date(response.data.list[0].dt * 1000);
+    console.log(localDate)
+    console.log(Math.round(response.data.list[0].main.temp-273.15))
+    console.log(Math.round(response.data.list[0].main.temp_min-273.15))
+    console.log(Math.round(response.data.list[0].main.temp_max-273.15))
+    // console.log(response.data.list[0].weather)
+    console.log(response.data.list[0].clouds.all + '%')
+    //console.log(response.data.list[0].rain.3h)
+    console.log(response.data)
+  })
+  .catch(error => {
+    console.log(error);
+  });
 }
+
+
+}
+
 
 export default InfoContainer = withTracker(() => {
   return {
