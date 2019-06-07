54258ff313651220d1acba1e8a52dda719dac013
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-06-06 22:37:28 -0700

    Initial layout for Info component

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -12,6 +12,22 @@ class Info extends Component {
       <div>
         <h2>Learn Meteor!</h2>
         <ul>{ links }</ul>
+        <h2>Your Next Run</h2>
+        <form>
+          Duration
+          <input type="text" name="duration" value="Hours: Minutes"></input>
+          <br/>
+          Start Time
+          <input type="text" name="start_time" value="Hours: Minutes"></input>
+          <br/>
+          End Time
+          <input type="text" name="end_time" value="Hours: Minutes"></input>
+          <br/>
+          Distance
+          <input type="text" name="distance" value="km/m"></input>
+          <br/>
+          <button type="submit">Find a Run!</button>
+        </form>
       </div>
     );
   }
