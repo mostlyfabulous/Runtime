de6cb22bd89fbebf980768e96804955034bbc9fc
Napat Karnsakultorn <karns.napat@gmail.com>
2019-06-11 17:57:00 -0700

    weather data now in redux

--- imports/ui/reducers/index.js
+++ imports/ui/reducers/index.js
@@ -1,14 +1,31 @@
 import { combineReducers } from 'redux';
 
-
 const weatherReducer = (weather, action) => {
   weather = weather || {};
 
 	if (action.type === 'ADD_WEATHER_DATA') {
     let newState = action.content;
-    console.log('cur_state0:' + JSON.stringify(weather));
     weather = newState;
-    console.log('cur_state1:' + JSON.stringify(weather));
+    //console.log('cur_state1:' + JSON.stringify(weather));
+    return weather;
+	}
+	return weather
+};
+
+const displayMessageReducer = (old='', action) => {
+	if (action.type === 'DISPLAY_MESSAGE') {
+    old = action;
+	}
+	return old;
+};
+
+const weatherReducer = (response, action) => {
+  weather = weather || {};
+
+	if (action.type === 'ADD_WEATHER_DATA') {
+    let newState = action.content;
+    weather = newState;
+    //console.log('cur_state1:' + JSON.stringify(weather));
     return weather;
 	}
 	return weather
