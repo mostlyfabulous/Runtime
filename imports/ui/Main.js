5eb583c8be4245f037ab104e3cf546b607f96b3e
Napat Karnsakultorn <karns.napat@gmail.com>
2019-06-20 14:30:25 -0700

    implemented conditional login on main component

--- imports/ui/Main.js
+++ imports/ui/Main.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import Info from './Info.js';
 import Calendar from './Calendar.js';
+import Login from './Login.js';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logged_in: true
+    };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleLoad();
+  }
     render() {
+      if (this.state.logged_in === false) {
+        return (
+            <div className='body'>
+                <div id='sideMenu'>
+                    <Info/>
+                </div>
+                <div id='loginForm'>
+                    <Login/>
+                </div>
+            </div>
+        )
+      } else {
         return (
             <div className='body'>
                 <div id='sideMenu'>
@@ -14,6 +36,7 @@ class Main extends React.Component {
                 </div>
             </div>
         )
+      }
     }
 }
 
