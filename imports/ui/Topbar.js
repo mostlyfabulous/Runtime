2af32adaf3ff6ae01e0fb75c288095d97f473536
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-06-22 12:04:44 -0700

    Initial top bar coloring behaviour

--- imports/ui/Topbar.js
+++ imports/ui/Topbar.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Topbar extends React.Component {
-    render() {
-        return (
-            <div className="topbar">Hello user{/* this.props.username */}!</div>
-        )
-    }
+
+  render() {
+    let t = null;
+    if (this.props.weather.data) t = this.props.weather.data.list[0].main.temp;
+    // warm
+    if (t > 298.15) return (
+        <div className="topbar warm">Hello user{/* this.props.username */}!</div>
+    )
+    // fair
+    else if (t > 295.15 && t <= 298.15) return (
+        <div className="topbar fair">Hello user{/* this.props.username */}!</div>
+    )
+    // cool
+    else if (t <= 295.15) return (
+        <div className="topbar cold">Hello user{/* this.props.username */}!</div>
+    )
+    return (
+        <div className="topbar">Hello user{/* this.props.username */}!</div>
+    )
+  }
+
+  // componentWillUpdate(nextProps, nextState) {
+  //   console.log(nextProps);
+  //   if (nextProps.weather.data.list[0].main.temp)
+  //     t = nextProps.weather.data.list[0].main.temp
+  //   }
 }
 
-export default Topbar;
+const mapStateToProps = (state) => {
+  return {  weather: state.weather,
+
+         };
+}
+
+export default connect(mapStateToProps)(Topbar);
