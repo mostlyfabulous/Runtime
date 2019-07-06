4188b7a79d06183d3c5aa812eef05ef662bd8e22
Napat <karns.napat@gmail.com>
2019-07-06 00:14:06 -0700

    unfinished userPref page

---
+++ imports/api/preferences.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export default Preferences = new Mongo.Collection('preferences');
--- server/users.js
+++ server/users.js
@@ -1,42 +1,61 @@
 import { Meteor } from 'meteor/meteor';
+import Preferences from '/imports/api/preferences';
 
-import { config } from '../config';
-
-
-const MongoClient = require('mongodb').MongoClient;
-let assert = require('assert');
-let Db = require('mongodb').Db;
-const uri = "mongodb+srv://"+config().mongoUser+":"+config().mongopw+"@sandbox-ifog1.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-
-client.connect(err => {
-  const collection = client.db("Runtime").collection("runs");
-  assert.equal(null, err);
-  insertUser(collection);
-  // perform actions on the collection object
-  //client.close();
+Meteor.publish('preferences', function() {
+ // args publish needs goes in: function(args)
+  if (!this.userId) {
+    console.log("No userId supplied");
+    return this.ready();
+  }
+  // console.log(Runs);
+  //Runs.find({owner: this.userId}).toArray(function(err, items) {
+  //   console.log("Returning any items found");
+  //   console.log(items);
+  //   console.log("End of items found");
+  // });
+  return Preferences.find({_id: this.userId});
 });
 
-function insertUser(collection) {
-  let newId = (Date.parse(new Date)).toString(16) + Math.floor(Math.random()*1000);
-  let user = {_id: newId, user: 'user', password: 'password' };
-  collection.insertOne(user);
-  setTimeout(function() {
-  // Fetch the document
-  collection.find().toArray(function(err, items) {
-     assert.equal(null, err);
-     assert.notEqual(0, items.length);
-     console.log(items);
-     // db.close();
-   });
- });
-}
 
-
-Meteor.startup(() => {
-});
-
-// Meteor.publish("user", function () {
-//   return Meteor.user();
+// import { Meteor } from 'meteor/meteor';
+//
+// import { config } from '../config';
+//
+//
+// const MongoClient = require('mongodb').MongoClient;
+// let assert = require('assert');
+// let Db = require('mongodb').Db;
+// const uri = "mongodb+srv://"+config().mongoUser+":"+config().mongopw+"@sandbox-ifog1.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+//
+//
+// client.connect(err => {
+//   const collection = client.db("Runtime").collection("runs");
+//   assert.equal(null, err);
+//   insertUser(collection);
+//   // perform actions on the collection object
+//   //client.close();
 // });
+//
+// function insertUser(collection) {
+//   let newId = (Date.parse(new Date)).toString(16) + Math.floor(Math.random()*1000);
+//   let user = {_id: newId, user: 'user', password: 'password' };
+//   collection.insertOne(user);
+//   setTimeout(function() {
+//   // Fetch the document
+//   collection.find().toArray(function(err, items) {
+//      assert.equal(null, err);
+//      assert.notEqual(0, items.length);
+//      console.log(items);
+//      // db.close();
+//    });
+//  });
+// }
+//
+//
+// Meteor.startup(() => {
+// });
+//
+// // Meteor.publish("user", function () {
+// //   return Meteor.user();
+// // });
