c09a4b82ed681a00f634d799b6371de8014355f8
root <root@LAPTOP-CCKO6TAQ.localdomain>
2019-07-06 12:48:30 -0700

    fixed history query error

--- imports/ui/containers/HistoryChartContainter.js
+++ imports/ui/containers/HistoryChartContainter.js
@@ -2,10 +2,15 @@ import { withTracker } from 'meteor/react-meteor-data';
 import Runs from '/imports/api/runs';
 import HistoryChart from '../components/HistoryChart.js'
 
+
+let date = new Date();
+let pastDate = new Date();
+pastDate.setDate(pastDate.getDate()-6);
+
 const HistoryChartContainer = withTracker( () => {
   const runHandle = Meteor.subscribe('week');
   const loadingRuns = !runHandle.ready();
-  const runs = Runs.find().fetch();
+  const runs = Runs.find({start: {$gte: pastDate, $lt: date}, owner: Meteor.user()._id}, {sort: {start: 1}}).fetch();
   const runsExists = !loadingRuns && !!runs;
   return {
     loadingRuns,
