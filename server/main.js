a9f3ff7d0991c883ad5edae4db0e4209409dec2b
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-07-15 23:29:55 -0700

    Update comments and tidying

--- server/main.js
+++ server/main.js
@@ -31,24 +31,23 @@ Meteor.startup(() => {
   hours = moment().get('h') + (moment().get('s')/60);
   gmt = hours+7
   console.log("current hour: " + hours);
+  // makes sure only the future 3 hour events are removed and not the current one
   gmt3HourBlock = (Math.ceil(gmt/3)*3)%24;
   console.log("GMT 3 Hour Block: " + gmt3HourBlock);
-  // makes sure only the future 3 hour events are removed and not the current one
   date3HourBlock = moment().add(7, 'hours'); // set to correct day—could be a day ahead
   date3HourBlock.hour(gmt3HourBlock).minute(0).second(0) // set to correct 3 hour block
   date3HourBlock = date3HourBlock.format();
   if ((elapsedHours) >= 3) {
     console.log("Time point to remove weather events from: " + date3HourBlock);
-    // weather seems to be given at GMT00:00 and so the api only returns
+    // weather is given at GMT00:00 and so the api only returns
     // 3 hour block events that are 7 hours ahead of us
     Weather.remove({start: {$gte: date3HourBlock}}, function(err, count) {
       console.log("Removed: " + count + " weather events");
     });
   }
-  // if 0 then we are at the latest event in the DB and should add new events
-  // if +ve then DB should be sent new weather events via api call
+  // if 0 then we are at the latest event in the DB and could add new events
+  // if >3 hours then DB should be sent new weather events via api call
 
-  // console.log(sortedEvents);
   if (Weather.find().count() === 0 || (elapsedHours) >= 3) {
     // let event = {"start":"2019-07-04T03:00:00-07:00","end":"2019-07-04T06:00:00-07:00","rendering":"background","color":"yellow","editable":false}
     // Weather.insert(event);
@@ -65,9 +64,7 @@ Meteor.startup(() => {
              else if (t <= 295.15) c = 'yellow'; // cool
              else (console.log(t))
              let e =  ({
-               // start: moment(threeHourEvent.dt_txt+" GMT"),
-               // end: moment(threeHourEvent.dt_txt+" GMT-0300"),
-               start: moment(threeHourEvent.dt_txt).format(),
+               start: moment(threeHourEvent.dt_txt).format(), // takes local time zone
                end: moment(threeHourEvent.dt_txt).add(3, 'hours').format(),
                city: cityData.name,
                countryCode: cityData.country,
@@ -94,8 +91,3 @@ Meteor.startup(() => {
   }
 
 });
-
-// Meteor.publish('user', function(){
-//     var currentUsername = this.userId;
-//     return PlayersList.find({userId: currentUsername});
-// });
