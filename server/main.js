1899f88f20ff7cd71eab5301219c784585115dc2
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-07-15 21:38:53 -0700

    Potential fix for weather events being updated
    Calculating the 3 hour block now uses minutes. Changed to $gte instead of $gt to remove events

--- server/main.js
+++ server/main.js
@@ -18,7 +18,7 @@ Meteor.startup(() => {
   const options = { sort: { start: -1 } };
   const sortedEvents = Weather.find(query, options).fetch();
   // get number of elapsed hours since last weatherEvent in DB
-  const elapsedHours = moment().diff(moment(sortedEvents[0].start), 'hours');
+  const elapsedHours = 120+moment().diff(moment(sortedEvents[0].start), 'hours');
   console.log("Hours since last weather update: " + (elapsedHours));
   console.log("Start of furthest event: " + moment(sortedEvents[0].start).format());
   console.log("End of furthest event: " + moment(sortedEvents[0].end).format());
@@ -28,7 +28,7 @@ Meteor.startup(() => {
   // and we should update those overlapping events with new data whilst adding
   // new events not in the DB
   console.log("Time now: " + moment().format());
-  hours = moment().get('hours');
+  hours = moment().get('h') + (moment().get('s')/60);
   gmt = hours+7
   console.log("current hour: " + hours);
   next3HourBlock = ((Math.ceil(hours/3)+2)*3)%24; // don't remove nearest 3 weather blocks (inclusive)
@@ -37,11 +37,11 @@ Meteor.startup(() => {
   console.log("next 3 Hour Block: " + next3HourBlock);
   // makes sure only the future 3 hour events are removed and not the current one
   date3HourBlock = moment().hour(gmt3HourBlock).minute(0).second(0).format();
-  if ((120+elapsedHours) < -5) {
+  if ((elapsedHours) < -6) {
     console.log("Time point to remove weather events from: " + date3HourBlock);
     // weather seems to be given at GMT00:00 and so the api only returns
     // 3 hour block events that are 7 hours ahead of us
-    Weather.remove({start: {$gt: date3HourBlock}}, function(err, count) {
+    Weather.remove({start: {$gte: date3HourBlock}}, function(err, count) {
       console.log("Removed: " + count + " weather events");
     });
   }
@@ -49,7 +49,7 @@ Meteor.startup(() => {
   // if +ve then DB should be sent new weather events via api call
 
   // console.log(sortedEvents);
-  if (Weather.find().count() === 0 || (120+elapsedHours) < -5 || elapsedHours >= 0) {
+  if (Weather.find().count() === 0 || (elapsedHours) < -6 || elapsedHours >= 0) {
     // let event = {"start":"2019-07-04T03:00:00-07:00","end":"2019-07-04T06:00:00-07:00","rendering":"background","color":"yellow","editable":false}
     // Weather.insert(event);
     let weatherkey = config().openweatherapi
