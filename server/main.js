03ccdf9b06d06ff1d0e106c419e4502ba5739ff2
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-07-20 21:15:04 -0700

    Generalize local time zone for weather events to work while in dev

--- server/main.js
+++ server/main.js
@@ -20,43 +20,43 @@ Meteor.startup(() => {
   const options = { sort: { start: -1 } };
   const sortedEvents = Weather.find(query, options).fetch();
   // get number of elapsed hours since last weatherEvent in DB
-  const elapsedHours = 127 + moment().diff(moment(sortedEvents[0].start), 'hours', true);
+  console.log("Time now: " + moment().format());
+  hours = moment().get('h') + (moment().get('s')/60);
+  timeDifference = moment().utcOffset()/60 // get hours ahead/behind UTC
+  console.log("Time difference is: " + timeDifference + " hours");
+  const elapsedHours = 120 - timeDifference + moment().diff(moment(sortedEvents[0].start), 'hours', true);
   console.log("Hours since last weather update: " + (elapsedHours));
   console.log("Start of furthest event: " + moment(sortedEvents[0].start).format());
   console.log("End of furthest event: " + moment(sortedEvents[0].end).format());
   console.log("Start of closest event: " + moment(sortedEvents[39].start).format());
   console.log("End of closest event: " + moment(sortedEvents[39].end).format());
 
-  console.log("Time now: " + moment().format());
-  hours = moment().get('h') + (moment().get('s')/60);
-  gmt = hours+7
-  console.log("current hour: " + hours);
   // makes sure only the future 3 hour events are removed and not the current one
-  gmt3HourBlock = (Math.ceil(gmt/3)*3)%24;
-  console.log("GMT 3 Hour Block: " + gmt3HourBlock);
-  date3HourBlock = moment().add(7, 'hours'); // set to correct day: could be a day ahead
-  date3HourBlock.hour(gmt3HourBlock).minute(0).second(0) // set to correct 3 hour block
+  utc3HourBlock = Math.ceil(((hours-timeDifference)%24)/3)*3;
+  console.log("UTC 3 Hour Block: " + utc3HourBlock);
+  date3HourBlock = moment().add(-timeDifference, 'hours'); // set to correct day: could be a day ahead
+  date3HourBlock.hour(utc3HourBlock).minute(0).second(0) // set to correct 3 hour block
   date3HourBlock = date3HourBlock.format();
   if ((elapsedHours) >= 3) {
     console.log("Time point to remove weather events from: " + date3HourBlock);
     /*
-    // DB will contain some overlapping events with a weather api call
+    // DB will contain some overlapping events with a weather API call
     // and we should update those overlapping events with new data whilst adding
     // new events not in the DB.
-    // Weather is given at GMT00:00 and so the api only returns
-    // 3 hour block events that are 7 hours ahead of us
+    // Weather is given at UTC and so the API only returns
+    // 3 hour block events that are 7 hours ahead of us (PST)
     */
     Weather.remove({start: {$gte: date3HourBlock}}, function(err, count) {
       console.log("Removed: " + count + " weather events");
     });
   }
   // if 0 then we are at the latest event in the DB and could add new events
-  // if >3 hours then DB should be sent new weather events via api call
+  // if >3 hours then DB should be sent new weather events via API call
 
   if (Weather.find().count() === 0 || (elapsedHours) >= 3) {
     // let event = {"start":"2019-07-04T03:00:00-07:00","end":"2019-07-04T06:00:00-07:00","rendering":"background","color":"yellow","editable":false}
     // Weather.insert(event);
-    let weatherkey = config().openweatherapi
+    let weatherkey = process.env.OPENWEATHER_API_TOKEN;
     axios.get('https://api.openweathermap.org/data/2.5/forecast?q=Vancouver,ca&appid=' + weatherkey)
       .then(response => {
         let cityData = response.data.city;
