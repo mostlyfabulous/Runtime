bd10d23e63abf666538c573a4a52ebe3e805eb2a
andrchoi <andrew.tt.choi@gmail.com>
2019-07-22 15:55:47 -0700

    added forecast to topbar

--- imports/ui/components/app/Topbar.js
+++ imports/ui/components/app/Topbar.js
@@ -1,53 +1,73 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withAccount } from '../accounts/connector.js'
+import AccountsUIWrapper from '../accounts/AccountsUIWrapper.js';
 
 class Topbar extends React.Component {
+  topbarInfo() {
+    let weatherInfo = ""
+    let city = "";
+    if (this.props.weather.data){
+      let data = this.props.weather.data;
+      console.log(data)
+
+      let temp = Math.round(data.list[0].main.temp-273.15) + '°C';
+      let temp_min = Math.round(data.list[0].main.temp_min-273.15) + '°C';
+      let temp_max = Math.round(data.list[0].main.temp_max-273.15) + '°C';
+      let clouds = data.list[0].clouds.all + '%';
+      city = data.city.name+", "+data.city.country;
+
+      weatherInfo = <div>
+        <p><b>Currently</b> {temp}</p>
+        <p><b>Low </b> {temp_min}, <b>High </b>{temp_max}</p>
+        <p><b>Cloud Coverage:</b> {clouds}</p>
+      </div>;
+    }
+    return <div>
+        <div className = 'topbarCity'>{city}</div>
+        <div className = 'topbarWeather'>{weatherInfo}</div>
+        <div className = 'topbarLogin'><AccountsUIWrapper/></div>
+      </div> 
+  }
 
   render() {
     // console.log(this.props);
     let user = "...logging in...";
     let baseUrl = 'https://robohash.org/';
-    if (this.props.account.user) user = this.props.account.user.username
+    if (this.props.account.user) user = this.props.account.user.username;
 
     let t = null;
+    let result;
     if (this.props.weather.data) t = this.props.weather.data.list[0].main.temp;
 
     if (user !== "...logging in...") {
       let picture = baseUrl + this.props.account.user._id;
       // warm
-      if (t > 298.15) return (
-          <div className="topbar warm">Hello {user}! <img src={picture} alt="Profile Icon" width="60" height="60"/> </div>
-      )
+      if (t > 298.15) 
+        result = <div className="topbar warm">Hello {user}! <img src={picture} alt="Profile Icon" width="60" height="60"/> {this.topbarInfo()} </div>
+      
       // fair
-      else if (t > 295.15 && t <= 298.15) return (
-          <div className="topbar fair">Hello {user}! <img src={picture} alt="Profile Icon" width="60" height="60"/></div>
-      )
+      else if (t > 295.15 && t <= 298.15)
+          result = <div className="topbar fair">Hello {user}! <img src={picture} alt="Profile Icon" width="60" height="60"/>{this.topbarInfo()} </div>
       // cool
-      else if (t <= 295.15) return (
-          <div className="topbar cold">Hello {user}! <img src={picture} alt="Profile Icon" width="60" height="60"/></div>
-      )
-      return (
-          <div className="topbar">Hello {user}! <img src={picture} alt="Profile Icon" width="60" height="60"/></div>
-      )
+      else if (t <= 295.15) 
+          result = <div className="topbar cold">Hello {user}! <img src={picture} alt="Profile Icon" width="60" height="60"/>{this.topbarInfo()} </div>
+      else 
+          result = <div className="topbar">Hello {user}! <img src={picture} alt="Profile Icon" width="60" height="60"/>{this.topbarInfo()} </div>
     } else {
       // warm
-      if (t > 298.15) return (
-          <div className="topbar warm">Hello {user}!</div>
-      )
+      if (t > 298.15) 
+          result = <div className="topbar warm">Hello {user}!{this.topbarInfo()} </div>
       // fair
-      else if (t > 295.15 && t <= 298.15) return (
-          <div className="topbar fair">Hello {user}!</div>
-      )
+      else if (t > 295.15 && t <= 298.15) 
+          result = <div className="topbar fair">Hello {user}!{this.topbarInfo()} </div>
       // cool
-      else if (t <= 295.15) return (
-          <div className="topbar cold">Hello {user}!</div>
-      )
-      return (
-          <div className="topbar">Hello {user}!</div>
-      )
+      else if (t <= 295.15) 
+          result = <div className="topbar cold">Hello {user}!{this.topbarInfo()} </div>
+      else
+          result = <div className="topbar">Hello {user}!{this.topbarInfo()} </div>
     }
-
+    return result;
   }
 }
 
