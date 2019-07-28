2ae9961503b089981fbd8fb0886495bbfbffdb21
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-07-27 18:28:09 -0700

    #27 Fix user prefs not being created for new user

--- server/preferences.js
+++ server/preferences.js
@@ -8,16 +8,10 @@ Meteor.publish('preferences', function() {
     return Preferences.find({_id: "defaultUser"});
   }
   if (Preferences.find({_id: this.userId}).fetch().length === 0) {
-  let pref = {_id: "defaultUser", clouds: 50, min_temp: 10, max_temp: 20, precipitation: 25, city: 'Vancouver'};
+  let pref = {_id: this.userId, clouds: 50, min_temp: 10, max_temp: 20, precipitation: 25, city: 'Vancouver'};
     Preferences.insert(pref);
   }
-  //console.log(this.userId);
-  // console.log('edit user pref');
-  // console.log(Meteor.user().userId);
-  // let newPref = {min_temp: 10, max_temp: 20};
-  // Preferences.update({_id:this.userId}, { $set: {pref: newPref} });
   return Preferences.find({_id: this.userId});
-  //return Preferences.find();
 });
 
 Meteor.methods({
@@ -33,9 +27,3 @@ Meteor.methods({
       })
   }
 });
-
-
-// const handle = Meteor.subscribe('user.runs', args);
-// when passing args is optional and used
-// to pass a parameter to publish
-// https://guide.meteor.com/data-loading.html#fetching
